3beb7159aee053c7f917c7c1795b6f3ac6297c3f
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-09-06 22:14:12 -0600

    Update Singleton.cs

--- Platformer Template/Assets/Scripts/GlobalScripts/Singleton.cs
+++ Platformer Template/Assets/Scripts/GlobalScripts/Singleton.cs
@@ -9,6 +9,8 @@ public class Singleton : MonoBehaviour
     
     public GameManager Game { get; private set; }
     public AudioManager Audio { get; private set; }
+
+    // use this for all random variables in the game. Keeps the backend from making multiple System.Randoms. Access this by typing Singleton.Global.Random
     public System.Random Random { get; private set; } = new System.Random();
 
     private void Awake()
@@ -22,8 +24,10 @@ public class Singleton : MonoBehaviour
             Global = this;
             DontDestroyOnLoad(this.gameObject);
         }
+
         Game = GetComponentInChildren<GameManager>();
         Audio = GetComponentInChildren<AudioManager>();
+
         if (Game == null)
         {
             Debug.LogError("Singleton must have a child with the GameManager script.");
