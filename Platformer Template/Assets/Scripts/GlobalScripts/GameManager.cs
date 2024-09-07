4c556b020ab9c9de30a58d9933ce309be72d0bc6
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-09-06 22:33:50 -0600

    Added small comment to each

--- Platformer Template/Assets/Scripts/GlobalScripts/GameManager.cs
+++ Platformer Template/Assets/Scripts/GlobalScripts/GameManager.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // use this class to take care of all in game problems that might arrise such as keeping track of the time spent in the level or the number of keys collected, etc.
+    
 }
