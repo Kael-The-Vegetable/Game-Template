b72909291a6daf574706c07ad5ff443ad693cfbc
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-09-12 15:36:19 -0600

    Removal of previous template

--- Platformer Template/Assets/Scripts/Interfaces.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageable
-{
-    internal int Health { get; set; }
-    public void TakeDamage(int damage);
-}
